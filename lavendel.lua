6fdf9daa1bf305a09b905622d7453fe861b91425
veryz-sillyz-boyz <ranxlus24@gmail.com>
2025-05-23 13:27:09 +0100

    Update lavendel.lua

--- lavendel.lua
+++ lavendel.lua
@@ -1,64 +1,62 @@
 return {
-    Scripts = {
-        {
-            Name = "Infinite Yield",
-            URL = "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"
-        },
-        {
-            Name = "Nameless Admin",
-            URL = "https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source"
-        },
-        {
-            Name = "Quirky CMD",
-            URL = "https://raw.githubusercontent.com/veryz-sillyz-boyz/rbx-utils/refs/heads/main/qCMD.lua"
-        },
-        {
-            Name = "Admin Enabled",
-            URL = "https://raw.githubusercontent.com/FilteringEnabled/FE/refs/heads/main/AdminEnabled"
-        },
-        {
-            Name = "Rockys Admin",
-            URL = "https://raw.githubusercontent.com/FilteringEnabled/FE/refs/heads/main/rockysadmin"
-        },
-        {
-            Name = "HatHub",
-            URL = "https://raw.githubusercontent.com/inkdupe/hat-scripts/refs/heads/main/updatedhathub.lua"
-        },
-        {
-            Name = "Moonlight Backdoor Scanner",
-            URL = "https://raw.githubusercontent.com/veryz-sillyz-boyz/rbx-utils/refs/heads/main/moonlight.lua"
-        },
-        {
-            Name = "Grow a Garden",
-            URL = "https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Garden/Garden-V1.lua"
-        },
-        {
-            Name = "Executor",
-            URL = "https://raw.githubusercontent.com/veryz-sillyz-boyz/rbx-utils/refs/heads/main/lavendel-executor.luau"
-        },
-        {
-            Name = "Polaria",
-            URL = "https://raw.githubusercontent.com/veryz-sillyz-boyz/rbx-utils/refs/heads/main/polaria.lua"
-        },
-        {
-            Name = "AnnaBypasser",
-            URL = "https://raw.githubusercontent.com/AnnaRoblox/AnnaBypasser/refs/heads/main/AnnaBypasser.lua"
-        },
-        {
-            Name = "SCP-096 FE",
-            URL = "https://pastefy.app/M25RnnGm/raw"
-        },
-        {
-            Name = "SLS Hitbox Expander",
-            URL = "https://raw.githubusercontent.com/veryz-sillyz-boyz/rbx-utils/refs/heads/main/SLS-HBE.lua"
-        },
-        {
-            Name = "Basketball Legends",
-            URL = "https://raw.githubusercontent.com/vnausea/absence-mini/refs/heads/main/absencemini.lua"
-        },
-        {
-            Name = "Lumber Tycoon 2",
-            URL = "https://raw.githubusercontent.com/DevKron/Kron_Hub/refs/heads/main/lt2new"
-        }
+    {
+        Name = "SCP-096 FE",
+        URL = "https://pastefy.app/M25RnnGm/raw"
+    },
+    {
+        Name = "Executor",
+        URL = "https://raw.githubusercontent.com/veryz-sillyz-boyz/rbx-utils/refs/heads/main/lavendel-executor.luau"
+    },
+    {
+        Name = "Lumber Tycoon 2",
+        URL = "https://raw.githubusercontent.com/DevKron/Kron_Hub/refs/heads/main/lt2new"
+    },
+    {
+        Name = "Moonlight Backdoor Scanner",
+        URL = "https://raw.githubusercontent.com/veryz-sillyz-boyz/rbx-utils/refs/heads/main/moonlight.lua"
+    },
+    {
+        Name = "HatHub",
+        URL = "https://raw.githubusercontent.com/inkdupe/hat-scripts/refs/heads/main/updatedhathub.lua"
+    },
+    {
+        Name = "SLS Hitbox Expander",
+        URL = "https://raw.githubusercontent.com/veryz-sillyz-boyz/rbx-utils/refs/heads/main/SLS-HBE.lua"
+    },
+    {
+        Name = "Nameless Admin",
+        URL = "https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source"
+    },
+    {
+        Name = "Grow a Garden",
+        URL = "https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Garden/Garden-V1.lua"
+    },
+    {
+        Name = "Infinite Yield",
+        URL = "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"
+    },
+    {
+        Name = "AnnaBypasser",
+        URL = "https://raw.githubusercontent.com/AnnaRoblox/AnnaBypasser/refs/heads/main/AnnaBypasser.lua"
+    },
+    {
+        Name = "Admin Enabled",
+        URL = "https://raw.githubusercontent.com/FilteringEnabled/FE/refs/heads/main/AdminEnabled"
+    },
+    {
+        Name = "Polaria",
+        URL = "https://raw.githubusercontent.com/veryz-sillyz-boyz/rbx-utils/refs/heads/main/polaria.lua"
+    },
+    {
+        Name = "Rockys Admin",
+        URL = "https://raw.githubusercontent.com/FilteringEnabled/FE/refs/heads/main/rockysadmin"
+    },
+    {
+        Name = "Basketball Legends",
+        URL = "https://raw.githubusercontent.com/vnausea/absence-mini/refs/heads/main/absencemini.lua"
+    },
+    {
+        Name = "Quirky CMD",
+        URL = "https://raw.githubusercontent.com/veryz-sillyz-boyz/rbx-utils/refs/heads/main/qCMD.lua"
     }
 }
